e65adaf38a9f71ecb9eaa3b1ab00fc6eb14a4939
Diego Gean da Fé <diegogeandafe@gmail.com>
2020-05-20 21:07:20 -0300

    Criado o Bean defaultSampler, para gerenciar o sistema de rastreamento distribuído

--- src/main/java/br/com/ecommerce/product/service/EcommerceProductServiceApplication.java
+++ src/main/java/br/com/ecommerce/product/service/EcommerceProductServiceApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @ComponentScan("br.com.ecommerce.product.service")
@@ -17,5 +20,10 @@ public class EcommerceProductServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceProductServiceApplication.class, args);
 	}
-
+	
+	@Bean
+	public Sampler defaultSampler(){
+		return Sampler.ALWAYS_SAMPLE;
+	}
+	
 }
